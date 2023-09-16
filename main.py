78772bfffb3ed43c1d56583ffc4181e91ed8a52b
Sreeram3927 <sreeram292004@gmail.com>
2023-09-16 19:56:19 +0530

    test get day order

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import request
+from flask import request as rq
 from flask import Response
 import json
 
@@ -8,6 +8,17 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return json.dumps({'message': 'Hello World'})
+
+@app.route('/day_order', methods=['POST'])
+def day_order():
+    if 'date' in rq.args:
+        date = rq.args.get('date')
+        return Response(json.dumps({'recieved': date}), status=200, mimetype='application/json')
+    else:
+        response = {"status": "error", "msg": "Error in Input Parameters"}
+        response = json.dumps(response)
+        response = Response(response, status=200, mimetype='application/json')
+        return response
 
 if __name__ == '__main__':
     app.run(debug=True)
