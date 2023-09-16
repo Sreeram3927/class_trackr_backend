ef58623ac91652713fd838da9ea48918b4025a03
Sreeram3927 <sreeram292004@gmail.com>
2023-09-16 20:15:05 +0530

    implement get_dayorder api

--- main.py
+++ main.py
@@ -2,23 +2,26 @@ from flask import Flask
 from flask import request as rq
 from flask import Response
 import json
+import day_order_dates
 
 app = Flask(__name__)
+dod = day_order_dates.DayOrderDates()
 
 @app.route('/')
 def home():
-    return json.dumps({'message': 'Hello World'})
+    return json.dumps({"message": "Hello World"})
 
 @app.route('/day_order', methods=['POST'])
 def day_order():
     if 'date' in rq.args:
-        date = rq.args.get('date')
-        return Response(json.dumps({'recieved': date}), status=200, mimetype='application/json')
+        data = dod.getDayOrder(rq.args.get('date'))
+        return Response(json.dumps({"day_order": data}), status=200, mimetype='application/json')
     else:
-        response = {"status": "error", "msg": "Error in Input Parameters"}
-        response = json.dumps(response)
-        response = Response(response, status=200, mimetype='application/json')
+        data = json.dumps({"status": "error", "msg": "Error in Input Parameters"})
+        response = Response(data, status=200, mimetype='application/json')
         return response
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
