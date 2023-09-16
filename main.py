a85c5d048e7a2d93b198154a674aa3c690a5ae54
Sreeram3927 <sreeram292004@gmail.com>
2023-09-16 19:30:45 +0530

    flask hello world

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print('Hello World')
+from flask import Flask
+from flask import request
+from flask import Response
+import json
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return json.dumps({'message': 'Hello World'})
+
+if __name__ == '__main__':
+    app.run(debug=True)
